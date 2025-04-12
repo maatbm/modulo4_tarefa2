c5bf93504f28bb0457965edb4b03e57090bf4b59
maatbm <matheusbmoraes0@gmail.com>
2025-04-12 11:09:56 -0300

    fix: code completed

--- src/main/java/Exercise_2.java
+++ src/main/java/Exercise_2.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Exercise_2 {
+    public static void main(String[] args) {
+        try(Scanner sc = new Scanner(System.in)){
+            System.out.println("Insira um número:");
+            String number = sc.next();
+
+            int numberConverted = Integer.parseInt(number);
+
+            System.out.println("Número convertido: " + numberConverted);
+        }catch (NumberFormatException e ){
+            System.err.println("Por favor, insira apenas números");
+        }catch (NullPointerException e){
+            System.err.println("Por favor, insira apenas números inteiros");
+        }
+    }
 }
